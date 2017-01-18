694b735cfd0381c7ff90085e2084c68e6c5311a4
Danny Pernik <dannypernik@gmail.com>
2017-01-18 01:21:27 -0600

    all urls and views added

--- openCurrents/urls.py
+++ openCurrents/urls.py
@@ -9,6 +9,27 @@ urlpatterns = [
     url(r'^community$', views.CommunityView.as_view(), name='community'),
     url(r'^login$', views.LoginView.as_view(), name='login'),
     url(r'^invite-friends$', views.InviteFriendsView.as_view(), name='invite-friends')
+    url(r'^approve-hours$', views.ApproveHoursView.as_view(), name='approve-hours'),
+	url(r'^causes$', views.CausesView.as_view(), name='causes'), 
+	url(r'^edit-hours$', views.EditHoursView.as_view(), name='edit-hours'),
+	url(r'^faq$', views.FaqView.as_view(), name='faq'),
+	url(r'^find-orgs$', views.FindOrgsView.as_view(), name='find-orgs'),
+	url(r'^give-currents$', views.GiveCurrentsView.as_view(), name='give-currents'),
+	url(r'^hours-approved$', views.HoursApprovedView.as_view(), name='hours-approved'),
+	url(r'^inventory$', views.InventoryView.as_view(), name='inventory'),
+	url(r'^nominate$', views.NominateView.as_view(), name='nominate'),
+	url(r'^nomination-confirmed$', views.NominationConfirmedView.as_view(), name='nomination-confirmed'),
+	url(r'^nomination-email$', views.NominationEmailView.as_view(), name='nomination-email'),
+	url(r'^org-account$', views.OrgAccountView.as_view(), name='org-account'),
+	url(r'^org-signup$', views.OrgSignupView.as_view(), name='org-signup'),
+	url(r'^request-currents$', views.RequestCurrentsView.as_view(), name='request-currents'),
+	url(r'^sell$', views.SellView.as_view(), name='sell'),
+	url(r'^signup$', views.SignupView.as_view(), name='signup'),
+	url(r'^tell-your-boss$', views.TellYourBossView.as_view(), name='tell-your-boss'),
+	url(r'^user-home$', views.UserHomeView.as_view(), name='user-home'),
+	url(r'^verify-identity$', views.VerifyIdentityView.as_view(), name='verify-identity'),
+	url(r'^volunteer-hours$', views.VolunteerHoursView.as_view(), name='volunteer-hours'),
+	url(r'^volunteer-requests$', views.VolunteerRequestsView.as_view(), name='volunteer-requests'),
 
     # functional views
     #url(r'^assign_credit_to_user/$', views.assign_credit_to_user, name='assign_credit_to_user')
--- openCurrents/views.py
+++ openCurrents/views.py
@@ -17,3 +17,66 @@ class LoginView(TemplateView):
 
 class InviteFriendsView(TemplateView):
     template_name = 'invite-friends.html'
+
+class ApproveHoursView(TemplateView):
+    template_name = 'approve-hours.html'
+
+class CausesView(TemplateView):
+    template_name = 'causes.html'
+
+class EditHoursView(TemplateView):
+    template_name = 'edit-hours.html'
+
+class FaqView(TemplateView):
+    template_name = 'faq.html'
+
+class FindOrgsView(TemplateView):
+    template_name = 'find-orgs.html'
+
+class GiveCurrentsView(TemplateView):
+    template_name = 'give-currents.html'
+
+class HoursApprovedView(TemplateView):
+    template_name = 'hours-approved.html'
+
+class InventoryView(TemplateView):
+    template_name = 'Inventory.html'
+
+class NominateView(TemplateView):
+    template_name = 'nominate.html'
+
+class NominationConfirmedView(TemplateView):
+    template_name = 'nomination-confirmed.html'
+
+class NominationEmailView(TemplateView):
+    template_name = 'nomination-email.html'
+
+class OrgHomeView(TemplateView):
+    template_name = 'org-home.html'
+
+class OrgSignupView(TemplateView):
+    template_name = 'org-signup.html'
+
+class RequestCurrentsView(TemplateView):
+    template_name = 'request-currents.html'
+
+class SellView(TemplateView):
+    template_name = 'sell.html'
+
+class SignupView(TemplateView):
+    template_name = 'signup.html'
+
+class TellYourBossView(TemplateView):
+    template_name = 'tell-your-boss.html'
+
+class UserHomeView(TemplateView):
+    template_name = 'user-home.html'
+
+class VerifyIdentityView(TemplateView):
+    template_name = 'verify-identity.html'
+
+class VolunteerHoursView(TemplateView):
+    template_name = 'volunteer-hours.html'
+
+class VolunteerRequestsView(TemplateView):
+    template_name = 'volunteer-requests.html'
