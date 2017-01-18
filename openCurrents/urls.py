7c385a727af5be8d3911d0cd30d6877d8652f9ed
Nickolay Shestopalov <nickolay@edyn.com>
2017-01-17 20:00:41 -1000

    Add more templates

--- openCurrents/urls.py
+++ openCurrents/urls.py
@@ -5,6 +5,10 @@ urlpatterns = [
     # template views
     url(r'^$', views.HomeView.as_view(), name='root'),
     url(r'^home$', views.HomeView.as_view(), name='home'),
+    url(r'^confirm-account$', views.ConfirmAccountView.as_view(), name='confirm-account'),
+    url(r'^community$', views.CommunityView.as_view(), name='community'),
+    url(r'^login$', views.LoginView.as_view(), name='login'),
+    url(r'^invite-friends$', views.InviteFriendsView.as_view(), name='invite-friends')
 
     # functional views
     #url(r'^assign_credit_to_user/$', views.assign_credit_to_user, name='assign_credit_to_user')
--- openCurrents/views.py
+++ openCurrents/views.py
@@ -5,3 +5,15 @@ from django.views.generic import View, TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class ConfirmAccountView(TemplateView):
+    template_name = 'confirm-account.html'
+
+class CommunityView(TemplateView):
+    template_name = 'community.html'
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class InviteFriendsView(TemplateView):
+    template_name = 'invite-friends.html'
