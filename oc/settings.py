703fd4eb5487d039036efbc420bfe8227bacb173
Nickolay Shestopalov <nickolay@edyn.com>
2017-01-21 01:10:47 -1000

    Change links

--- oc/settings.py
+++ oc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k)2cm1xy=m9zwyvqj9xw@0pe(fnzlmtq&x6xzk@@em2$590_wg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'openCurrents.apps.OpencurrentsConfig',    
+    'openCurrents.apps.OpencurrentsConfig',
 ]
 
 MIDDLEWARE = [
