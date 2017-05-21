dacd60eabb29c917c0ed5fd7fe979c0a08066840
Nickolay Shestopalov <nickolay@edyn.com>
2017-05-20 22:23:24 -0700

    Hour tracking models

--- openCurrents/models.py
+++ openCurrents/models.py
@@ -4,8 +4,51 @@ from uuid import uuid4
 
 from django.db import models
 
+# Notes:
+# *) unverified users are still created as User objects but with unusable password
+
 def one_week_from_now():
     return timezone.now() + timedelta(days=7)
+
+# org model
+class Org(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    website = models.CharField(max_length=100, unique=True, null=True)
+    status = models.CharField(max_length=50, null=True)
+    mission = models.CharField(max_length=4096, null=True)
+    reason = models.CharField(max_length=4096, null=True)
+    users = models.ManyToManyField(User, through='OrgUser')
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+    def __unicode__(self):
+        return ' '.join([
+            str(self.status),
+            str(self.name)
+        ])
+
+
+# user-org affiliations
+class OrgUser(models.Model):
+    user = models.ForeignKey(User)
+    org = models.ForeignKey(Org)
+    affiliation = models.CharField(max_length=50, null=True)
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'org')
+
+    def __unicode__(self):
+        return ' '.join([
+            'User',
+            self.user.email,
+            'is',
+            str(self.affiliation),
+            'at',
+            self.org.name
+        ])
+
 
 class Account(models.Model):
     user = models.OneToOneField(User)
@@ -20,6 +63,40 @@ class Account(models.Model):
         return ' '.join([self.user.username, '\'s account'])
 
 
+class Project(models.Model):
+    name = models.CharField(max_length=1024)
+    org = models.ForeignKey(Org)
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+
+class Event(models.Model):
+    project = models.ForeignKey(Project)
+    location = models.CharField(max_length=1024)
+    date_start = models.DateTimeField('start date')
+    date_end = models.DateTimeField('end date')
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+
+class UserEventRegistration(models.Model):
+    event = models.ForeignKey(Event)
+    user = models.ForeignKey(User)
+    confirmed = models.BooleanField(default=False)
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+
+class UserTimeLog(models.Model):
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
+    date_start = models.DateTimeField('start time')
+    date_end = models.DateTimeField('end time')
+    verified = models.BooleanField(default=False)
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+
+
 # verification tokens
 class Token(models.Model):
     email = models.EmailField()
@@ -31,44 +108,11 @@ class Token(models.Model):
         'date invite token expires',
         default=one_week_from_now
     )
+    date_created = models.DateTimeField('date created', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
 
     def __unicode__(self):
         return ' '.join([
             'Verification token for',
             self.email
-        ])
-
-# org model
-class Org(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    website = models.CharField(max_length=100, unique=True, null=True)
-    status = models.CharField(max_length=50, null=True)
-    mission = models.CharField(max_length=4096, null=True)
-    reason = models.CharField(max_length=4096, null=True)
-    users = models.ManyToManyField(User, through='OrgUser')
-
-    def __unicode__(self):
-        return ' '.join([
-            str(self.status),
-            str(self.name)
-        ])
-
-
-# user-org affiliations
-class OrgUser(models.Model):
-    user = models.ForeignKey(User)
-    org = models.ForeignKey(Org)
-    affiliation = models.CharField(max_length=50, null=True)
-
-    class Meta:
-        unique_together = ('user', 'org')
-
-    def __unicode__(self):
-        return ' '.join([
-            'User',
-            self.user.email,
-            'is',
-            str(self.affiliation),
-            'at',
-            self.org.name
         ])
