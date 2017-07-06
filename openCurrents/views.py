530628cd0d68eaa53b812e6e4a248082c01a3b7e
Tim Godwin <tegodwin@gmail.com>
2017-07-05 20:50:57 -0500

    Adding monthly_update boolean support in models.py Account class.
    Adding monthly_update checkbox form input in confirm-account.html template.
    
    Adding conditional check on form monthly_update variable to set user_account boolean in views.py

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -804,6 +804,12 @@ def process_email_confirmation(request, user_email):
 
         # create user account
         user_account = Account(user=user, pending=1)
+
+        if form.cleaned_data['monthly_updates']:
+            user_account.monthly_updates = True;
+        else:
+            user_account.monthly_updates = False;
+
         user_account.save()
 
         # add credit to the referrer
