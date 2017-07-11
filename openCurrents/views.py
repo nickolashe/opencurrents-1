2fcda5c4166a7ff7398953a8e69b8ffcafc29243
Danny Pernik <dannypernik@gmail.com>
2017-07-11 16:36:38 -0500

    change redirect from user-home to profile

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -877,7 +877,7 @@ def process_email_confirmation(request, user_email):
 
         if user.has_usable_password():
             logger.warning('user %s has already been verified', user_email)
-            return redirect('openCurrents:user-home')
+            return redirect('openCurrents:profile')
 
         # second, make sure the verification token and user email match
         token_record = None
@@ -897,7 +897,7 @@ def process_email_confirmation(request, user_email):
 
         if token_record.is_verified:
             logger.warning('token for %s has already been verified', user_email)
-            return redirect('openCurrents:user-home')
+            return redirect('openCurrents:profile')
 
         # mark the verification record as verified
         token_record.is_verified = True
@@ -969,7 +969,7 @@ def process_email_confirmation(request, user_email):
 
         except:
             logger.info('No org association')
-            return redirect('openCurrents:user-home')
+            return redirect('openCurrents:profile')
 
     #if form was invalid for bad password, still need to preserve token 
     else:
@@ -1047,7 +1047,7 @@ def process_org_signup(request):
             request.user.email
         )
         return redirect(
-            'openCurrents:user-home',
+            'openCurrents:profile',
             status_msg='Thank you for nominating %s to openCurrents!' % org.name
         )
 
