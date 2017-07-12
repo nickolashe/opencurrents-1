b912965d1907e8c21f2fc45edc3a7916a996ce72
karbmk <karthikbm01@gmail.com>
2017-07-12 15:03:07 -0500

    added event type in Events model
    Signed-off-by: karbmk <karthikbm01@gmail.com>

--- openCurrents/models.py
+++ openCurrents/models.py
@@ -108,6 +108,17 @@ class Event(models.Model):
     # coordinator contact info
     coordinator_firstname = models.CharField(max_length=128)
     coordinator_email = models.EmailField()
+    MANUAL = 'MN'
+    GROUP = 'GR'
+    OTHER = 'OT'
+    event_type_choices = (
+        (MANUAL, 'ManualTracking'),
+        (GROUP, 'Group'),
+        (OTHER, 'Other'),
+    )
+    event_type = models.CharField(max_length=2,
+                                      choices=event_type_choices,
+                                      default=GROUP)
 
     # start / end timestamps of the project
     datetime_start = models.DateTimeField('start datetime')
