707920bf7f85c79f2818511e740751fa796cc106
karbmk <karthikbm01@gmail.com>
2017-07-31 16:54:28 -0500

    Issue #228 resolved

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -711,6 +711,7 @@ class LiveDashboardView(LoginRequiredMixin, SessionContextView, TemplateView):
         )
         context['registered_users'] = registered_users
 
+
         # non-registered (existing) users
         unregistered_users = [
             ur_user
@@ -719,7 +720,6 @@ class LiveDashboardView(LoginRequiredMixin, SessionContextView, TemplateView):
             ])
         ]
         context['unregistered_users'] = unregistered_users
-
         # dict for looking up user data by lastname
         uu_lookup = dict([
             (user.last_name, {
@@ -981,13 +981,19 @@ def event_register_live(request, eventid):
     userid = request.POST['userid']
     user = User.objects.get(id=userid)
     event = Event.objects.get(id=eventid)
-    user_event_registration = UserEventRegistration(
-        user=user,
-        event=event,
-        is_confirmed=True
-    )
-    user_event_registration.save()
-    logger.info('User %s registered for event %s', user.username, event.id)
+    user_events = UserEventRegistration.objects.values('user__id','event__id').filter(user__id = userid)
+    user_event_ids = [d for d in user_events if int(userid) == d['user__id'] and int(eventid) == d['event__id']]
+    if not user_event_ids:
+        user_event_registration = UserEventRegistration(
+            user=user,
+            event=event,
+            is_confirmed=True
+        )
+        user_event_registration.save()
+        logger.info('User %s registered for event %s', user.username, event.id)
+    else:
+        logger.info('User %s already registered for event %s', user.username, event.id)
+        return HttpResponse(status=400)
     tz = event.project.org.timezone
     event_ds = event.datetime_start.astimezone(pytz.timezone(tz)).time()
     event_de = event.datetime_end.astimezone(pytz.timezone(tz)).time()
