3da4be6a195d9250700d51c29b259349221ab005
Tim Godwin <tegodwin@gmail.com>
2017-08-22 10:38:19 -0500

    adding back creator_id in event creation

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -898,6 +898,7 @@ class CreateEventView(OrgAdminPermissionMixin, SessionContextView, FormView):
             datetime_end=form_data['datetime_end'],
             coordinator_firstname=form_data['coordinator_firstname'],
             coordinator_email=form_data['coordinator_email'],
+            creator_id = self.userid
         )
         event.save()
         try:
