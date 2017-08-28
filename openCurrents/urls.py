30969abab72509708a2ca9afcd2234df43c57816
Danny Pernik <dannypernik@gmail.com>
2017-08-28 15:17:03 -0500

    styling and conditional logic for See registrants, invite-volunteer links working after changing event_ids to event_id in urls.py

--- openCurrents/urls.py
+++ openCurrents/urls.py
@@ -79,7 +79,7 @@ urlpatterns = [
     url(r'^create-event/(?P<org_id>\d+)/$', views.CreateEventView.as_view(), name='create-event'),
     url(r'^project-details$', views.ProjectDetailsView.as_view(), name='project-details'),
     url(r'^invite-volunteers$', views.InviteVolunteersView.as_view(), name='invite-volunteers'),
-    url(r'^invite-volunteers/(?P<event_ids>.+)/$', views.InviteVolunteersView.as_view(), name='invite-volunteers'),
+    url(r'^invite-volunteers/(?P<event_id>.+)/$', views.InviteVolunteersView.as_view(), name='invite-volunteers'),
     url(r'^volunteers-invited/(?P<vol_no>\d+)/$', views.VolunteersInvitedView.as_view(), name='volunteers-invited'),
     url(r'^event-created/(?P<project>\w+)/(?P<num_events>\d+)/$', views.EventCreatedView.as_view(), name='event-created'),
     url(r'^live-dashboard/(?P<event_id>\d+)/$',
