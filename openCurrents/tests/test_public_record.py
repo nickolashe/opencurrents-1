4a7c5aaed4f57c4d519dd0896bdf46c3df5a40c5
Danil <a35012a@gmail.com>
2017-11-24 16:36:52 +0700

    Fix unittests #740

--- openCurrents/tests/test_public_record.py
+++ openCurrents/tests/test_public_record.py
@@ -1,4 +1,5 @@
-from django.utils.timezone import datetime, timedelta
+from datetime import timedelta
+from django.utils import timezone
 import random
 import string
 
@@ -17,39 +18,39 @@ class PublicRecordViewTestSuite(TestCase):
         pass
 
     @staticmethod
-    def _generate_test_name():
-        return "".join(random.sample(string.ascii_lowercase, 8))
+    def _generate_test_name(prefix="", length=6):
+        return prefix + "".join(random.sample(string.ascii_lowercase, length))
 
-    def set_up_org(self, status, old=False):
-        org = OcOrg().setup_org(name=self._generate_test_name(), status=status)
+    def set_up_org(self, status, old=False, user=False):
+        org = OcOrg().setup_org(name=self._generate_test_name(prefix=status), status=status)
         user_org = OcUser().setup_user(
-            username=self._generate_test_name(),
-            email=self._generate_test_name(),
+            username=self._generate_test_name(prefix='username'),
+            email=self._generate_test_name(prefix='email'),
         )
         project = Project.objects.create(
             org=org,
-            name=self._generate_test_name()
+            name=self._generate_test_name(prefix='project')
         )
         event = Event.objects.create(
             project=project,
-            description=self._generate_test_name(),
-            location=self._generate_test_name(),
+            description=self._generate_test_name(prefix='desc'),
+            location=self._generate_test_name(prefix='loc'),
             coordinator=user_org,
             creator_id=user_org.id,
-            event_type=self._generate_test_name(),
-            datetime_start=datetime.now() - timedelta(hours=2),
-            datetime_end=datetime.now() - timedelta(hours=1)
+            event_type=self._generate_test_name(prefix='event_type'),
+            datetime_start=timezone.now() - timedelta(hours=2),
+            datetime_end=timezone.now() - timedelta(hours=1)
         )
         regular_user = OcUser().setup_user(
-            username=self._generate_test_name(),
-            email=self._generate_test_name(),
+            username=self._generate_test_name(prefix='username'),
+            email=self._generate_test_name(prefix='email'),
         )
         user_reg_time_log = UserTimeLog.objects.create(
             user=regular_user,
             event=event,
             is_verified=True,
-            datetime_start=datetime.now(),
-            datetime_end=datetime.now() + timedelta(hours=1)
+            datetime_start=timezone.now(),
+            datetime_end=timezone.now() + timedelta(hours=1)
         )
         regular_user_action = AdminActionUserTime.objects.create(
             user=user_org,
@@ -64,26 +65,36 @@ class PublicRecordViewTestSuite(TestCase):
                 regular_user.userentity.id,
                 regular_user_action,
                 amount,
-
             )
-        else:
+        elif status == 'biz':
             OcLedger().issue_currents(
-                regular_user.userentity.id,
                 org.orgentity.id,
+                regular_user.userentity.id,
                 regular_user_action,
                 amount,
-                entity_type_from='user',
-                entity_type_to='org'
+            )
+            OcLedger().transact_currents(
+                'user',
+                regular_user.userentity.id,
+                'org',
+                org.orgentity.id,
+                regular_user_action,
+                amount
             )
         if old:
             t = Ledger.objects.last()
-            t.date_created = datetime.now() - timedelta(days=33)
-            t.date_updated = datetime.now() - timedelta(days=33)
+            t.date_created -= timedelta(days=33)
             t.save()
-            self.old_top_with_names.append({'name': org.name, 'total': amount})
+            if user:
+                self.old_top_with_names.append({'name': regular_user.username, 'total': amount})
+            else:
+                self.old_top_with_names.append({'name': org.name, 'total': amount})
             self.old_top_with_names.sort(key=lambda x: x['total'], reverse=True)
         else:
-            self.top_with_names.append({'name': org.name, 'total': amount})
+            if user:
+                self.top_with_names.append({'name': regular_user.username, 'total': amount})
+            else:
+                self.top_with_names.append({'name': org.name, 'total': amount})
             self.top_with_names.sort(key=lambda x: x['total'], reverse=True)
 
     def test_view_displays_up_to_10_active_npf_last_month(self):
@@ -153,3 +164,43 @@ class PublicRecordViewTestSuite(TestCase):
         [self.set_up_org(status='biz', old=True) for _ in range(10)]
         top_npf_all_time = OcOrg().get_top_accepted_bizs('all-time')
         self.assertEqual(len(top_npf_all_time), 10)
+
+    def test_view_displays_up_to_10_active_users_last_month(self):
+        # Create 5 npfs
+        # Each of them has admin, project, event, user with timelog accepted by admin and issued transaction
+        [self.set_up_org(status='npf', user=True) for _ in range(5)]
+
+        # Random transaction amounts are recorded to the list
+        # And compared to interface output
+        top_users_last_month = OcUser().get_top_received_users('month')
+        # 10 users for 5 orgs is because each org contains admin
+        self.assertEqual(len(top_users_last_month), 10)
+        self.assertEqual(top_users_last_month[0], self.top_with_names[0])
+        # 5 last users are admins, they've got 0 earned currency
+        for x in range(5, 10):
+            self.assertEqual(top_users_last_month[x]['total'], 0)
+
+        # Create 10 more organisations to check that interface method outputs 10 items
+        [self.set_up_org(status='npf') for _ in range(10)]
+        top_users_last_month = OcUser().get_top_received_users('month')
+        self.assertEqual(len(top_users_last_month), 10)
+
+    def test_view_displays_up_to_10_active_users_all_time(self):
+        # Create 5 npfs
+        # Each of them has admin, project, event, user with timelog accepted by admin and issued transaction
+        [self.set_up_org(status='npf', user=True, old=True) for _ in range(5)]
+
+        # Random transaction amounts are recorded to the list
+        # And compared to interface output
+        top_users_last_month = OcUser().get_top_received_users('all-time')
+        # 10 users for 5 orgs is because each org contains admin
+        self.assertEqual(len(top_users_last_month), 10)
+        self.assertEqual(top_users_last_month[0], self.old_top_with_names[0])
+        # 5 last users are admins, they've got 0 earned currency
+        for x in range(5, 10):
+            self.assertEqual(top_users_last_month[x]['total'], 0)
+
+        # Create 10 more organisations to check that interface method outputs 10 items
+        [self.set_up_org(status='npf') for _ in range(10)]
+        top_users_last_month = OcUser().get_top_received_users('all-time')
+        self.assertEqual(len(top_users_last_month), 10)
