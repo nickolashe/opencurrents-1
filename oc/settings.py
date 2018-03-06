246ca88bec47e1099895629b1525dff9e72a8f80
Nickolay Shestopalov <nickolay@edyn.com>
2018-03-06 10:21:07 -0800

    prod mode

--- oc/settings.py
+++ oc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k)2cm1xy=m9zwyvqj9xw@0pe(fnzlmtq&x6xzk@@em2$590_wg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
