2783d0e24188bf164a26ffc9881f700091f1c4f0
tevtonez <tevtonez@gmail.com>
2018-03-07 00:13:01 +0100

    reverting db settigns

--- oc/settings.py
+++ oc/settings.py
@@ -194,24 +194,23 @@ except ImportError:
 
 # [START dbconfig]
 # (flexible)
-# DATABASES = {
-#     'default': {
-#         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
-#         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'opencurrents',
-#         'USER': 'oc_admin',
-#         'PASSWORD': '0pencu44',
-#         # For MySQL, set 'PORT': '3306' instead of the following. Any Cloud
-#         # SQL Proxy instances running locally must also be set to tcp:3306.
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
+        # 'ENGINE': 'django.db.backends.mysql' instead of the following.
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'opencurrents',
+        'USER': 'oc_admin',
+        'PASSWORD': '0pencu44',
+        # For MySQL, set 'PORT': '3306' instead of the following. Any Cloud
+        # SQL Proxy instances running locally must also be set to tcp:3306.
+        'PORT': '5432',
+    }
+}
 # In the flexible environment, you connect to CloudSQL using a unix socket.
 # Locally, you can use the CloudSQL proxy to proxy a localhost connection
 # to the instance
-# DATABASES['default']['HOST'] = '/cloudsql/opencurrents-194003:us-central1:oc-pg'
-
+DATABASES['default']['HOST'] = '/cloudsql/opencurrents-194003:us-central1:oc-pg'
 if os.getenv('GAE_INSTANCE'):
     pass
 else:
