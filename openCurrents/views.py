24d46b5db65173c0e088220b34fae15011d23b73
tevtonez <tevtonez@gmail.com>
2018-03-20 23:44:11 +0100

    #1153 - add send email to user w/o account when registering to event

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -3801,30 +3801,94 @@ def process_signup(
 
                 # registering user to an event
                 if 'next' in request.session and 'event-detail' in request.session['next']:
+                    try:
+                        user = User.objects.get(email=user_email)
+                    except:
+                        user = None
+                        logger.debug("Couldn't find user with email {}".format(user_email))
+
+                    if user:
+                        event_id = re.search('/(\d*)/', request.session['next']).group(1)
                         try:
-                            user = User.objects.get(email=user_email)
+                            event = Event.objects.get(id=event_id)
+                            user_event_registration = UserEventRegistration(
+                                user=user,
+                                event=event,
+                                is_confirmed=True
+                            )
+                            user_event_registration.save()
+
+                            # cleaning session
+                            request.session.pop('next')
+
                         except:
                             user = None
-                            logger.debug("Couldn't find user with email {}".format(user_email))
+                            logger.debug("Couldn't find event with ID {}".format(event_id))
 
-                        if user:
-                            event_id = re.search('/(\d*)/', request.session['next']).group(1)
+                        status_msg = 'You have been registered for ({})'.format(event)
+
+                        if event:
+                            logger.debug("Sending event reg confirm email")
+                            # sending event registration confirmation email to the new volunteer
+                            tz = event.project.org.timezone
+                            merge_var_list = [
+                                {
+                                    'name': 'EVENT_NAME',
+                                    'content': event.project.name
+                                },
+                                {
+                                    'name': 'DATE',
+                                    'content': event.datetime_start.astimezone(pytz.timezone(tz)).strftime('%b %d, %Y')
+                                },
+                                {
+                                    'name': 'START_TIME',
+                                    'content': event.datetime_start.astimezone(pytz.timezone(tz)).strftime('%-I:%M %p')
+                                },
+                                {
+                                    'name': 'END_TIME',
+                                    'content': event.datetime_end.astimezone(pytz.timezone(tz)).strftime('%-I:%M %p')
+                                },
+                                {
+                                    'name': 'LOCATION',
+                                    'content': event.location
+                                },
+                                {
+                                    'name': 'DESCRIPTION',
+                                    'content': event.description
+                                },
+                                {
+                                    'name': 'EVENT_ID',
+                                    'content': event.id
+                                },
+                                {
+                                    'name': 'ORG_NAME',
+                                    'content': event.project.org.name
+                                },
+                                {
+                                    'name': 'ADMIN_FIRSTNAME',
+                                    'content': event.coordinator.first_name
+                                },
+                                {
+                                    'name': 'ADMIN_LASTNAME',
+                                    'content': event.coordinator.last_name
+                                }
+                            ]
+
                             try:
-                                event = Event.objects.get(id=event_id)
-                                user_event_registration = UserEventRegistration(
-                                    user=user,
-                                    event=event,
-                                    is_confirmed=True
+                                sendContactEmail(
+                                    'volunteer-confirmation',
+                                    None,
+                                    merge_var_list,
+                                    user_email,
+                                    event.coordinator.email
                                 )
-                                user_event_registration.save()
+                            except Exception as e:
+                                logger.error(
+                                    'unable to send email: %s (%s)',
+                                    e.message,
+                                    type(e)
+                                )
 
-                                # cleaning session
-                                request.session.pop('next')
-                            except:
-                                user = None
-                                logger.debug("Couldn't find event with ID {}".format(event_id))
-
-                            status_msg = 'You have been registered for ({})'.format(event)
 
 
                 if not mock_emails:
