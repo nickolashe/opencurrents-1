9dbcf97c1d4bab4271bb1033bb7a04179cc3ff6e
Nickolay Shestopalov <nickolay@edyn.com>
2018-03-26 00:48:20 -0700

    change to oC

--- openCurrents/models.py
+++ openCurrents/models.py
@@ -629,7 +629,7 @@ class TransactionAction(models.Model):
 
             # transact usd from oC to user
             Ledger.objects.create(
-                entity_from=OrgEntity.objects.get(org__name='GreatDeeds'),
+                entity_from=OrgEntity.objects.get(org__name='openCurrents'),
                 entity_to=tr.user.userentity,
                 currency='usd',
                 amount=convert.cur_to_usd(tr.currents_amount, True),
