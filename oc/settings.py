86cce50e1dad381e4d2cfbf17961dc2334424a15
Nickolay Shestopalov <nickolay@edyn.com>
2018-03-31 02:02:55 -0700

    heroku

--- oc/settings.py
+++ oc/settings.py
@@ -149,7 +149,7 @@ LOGIN_URL = 'openCurrents:login'
 
 # [START dbconfig]
 # (flexible)
-if os.getenv['OC_HEROKU']:
+if os.getenv('OC_HEROKU'):
     # Update database configuration with $DATABASE_URL.
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
