a1231a7c34f5ecd7632351bc8c1d2e02e63be85b
Nickolay Shestopalov <nickolay@edyn.com>
2018-04-25 01:05:28 -0500

    import_export

--- openCurrents/admin.py
+++ openCurrents/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
-# Register your models here.
 from openCurrents.models import (
     Org,
     OrgUser,
@@ -22,7 +23,50 @@ from openCurrents.models import (
     Ledger
 )
 
-admin.site.register(Org)
+
+class LedgerResource(resources.ModelResource):
+    class Meta:
+        model = Ledger
+
+
+class LedgerAdmin(ImportExportModelAdmin):
+    resource_class = LedgerResource
+    # search_fields = ('entity_to__org__email',)
+
+
+class OrgResource(resources.ModelResource):
+    class Meta:
+        model = Org
+
+
+class OrgAdmin(ImportExportModelAdmin):
+    resource_class = OrgResource
+    search_fields = ('name',)
+
+
+class TransactionResource(resources.ModelResource):
+    class Meta:
+        model = Transaction
+
+
+class TransactionAdmin(ImportExportModelAdmin):
+    resource_class = TransactionResource
+    search_fields = (
+        'user__id', 'user__email', 'offer__id', 'offer__org__name', 'biz_name'
+    )
+
+
+class UserTimeLogResource(resources.ModelResource):
+    class Meta:
+        model = UserTimeLog
+
+
+class UserTimeLogAdmin(ImportExportModelAdmin):
+    resource_class = UserTimeLogResource
+    search_fields = ('user__id', 'user__email', 'event__id', 'event__name')
+
+
+admin.site.register(Org, OrgAdmin)
 admin.site.register(OrgUser)
 admin.site.register(Token)
 admin.site.register(Project)
@@ -30,13 +74,13 @@ admin.site.register(Event)
 admin.site.register(UserCashOut)
 admin.site.register(UserEventRegistration)
 admin.site.register(UserSettings)
-admin.site.register(UserTimeLog)
+admin.site.register(UserTimeLog, UserTimeLogAdmin)
 admin.site.register(AdminActionUserTime)
 admin.site.register(Offer)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Item)
 admin.site.register(TransactionAction)
 admin.site.register(Entity)
 admin.site.register(UserEntity)
 admin.site.register(OrgEntity)
-admin.site.register(Ledger)
+admin.site.register(Ledger, LedgerAdmin)
