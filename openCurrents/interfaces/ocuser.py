8f7d8ee44fd1cfe9342265d2928bf61ca4400151
Danny Pernik <dannypernik@gmail.com>
2018-05-04 13:15:57 -0500

    active offers in marketplace only

--- openCurrents/interfaces/ocuser.py
+++ openCurrents/interfaces/ocuser.py
@@ -243,7 +243,7 @@ class OcUser(object):
         get all offers in the marketplace
             - annotated by number of redeemed for given timeframe
         '''
-        offers_all = Offer.objects.filter(is_master=False).order_by('-date_updated')
+        offers_all = Offer.objects.filter(is_master=False).filter(is_active=True).order_by('-date_updated')
 
         for offer in offers_all:
             # logger.debug('%d: %d', offer.id, num_redeemed)
