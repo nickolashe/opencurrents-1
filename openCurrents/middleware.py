2c469f4146b43ac1dacd3ee1e399c8057d648b29
tevtonez <tevtonez@gmail.com>
2018-05-09 21:52:42 +0200

    #1297 fix redirection for logged out users, repeating the changes in middleware file

--- openCurrents/middleware.py
+++ openCurrents/middleware.py
@@ -1,27 +1,47 @@
 from django.conf import settings
 from django.contrib import auth
+from django.contrib import messages
 from datetime import datetime, timedelta
 from django.shortcuts import redirect
 from django.utils.deprecation import MiddlewareMixin
 
+
 class AutoLogout(MiddlewareMixin):
+
     def process_request(self, request):
-        if not request.user.is_authenticated() :
-          #Can't log out if not logged in
-          return
+
+        if not request.user.is_authenticated():
+            # Can't log out if not logged in
+            return
 
         try:
             time_now = datetime.strptime(datetime.now().isoformat(' '), "%Y-%m-%d %H:%M:%S.%f")
             time_sess = datetime.strptime(request.session['last_touch'], "%Y-%m-%d %H:%M:%S.%f")
             iddle_time = time_now - time_sess
 
-            if  iddle_time > timedelta(0, settings.AUTO_LOGOUT_DELAY * 60, 0):
+            if iddle_time > timedelta(0, settings.AUTO_LOGOUT_DELAY * 60, 0):
+                requested_page = request.META.get('PATH_INFO')
+
                 request.session.pop('last_touch')
                 auth.logout(request)
-                return redirect('openCurrents:home')
+
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    "You've been logged out after 1 hour of inactivity",
+                    extra_tags='alert'
+                )
+
+                # setting next page URL after login
+                # preventing from cycling trhrough logout url
+                if requested_page != '/process_logout/':
+                    request.session['next'] = requested_page
+                else:
+                    request.session['next'] = None
+                    requested_page = 'openCurrents:login'
+
+                return redirect(requested_page)
         except KeyError:
             pass
 
         request.session['last_touch'] = datetime.now().isoformat(' ')
-
-
