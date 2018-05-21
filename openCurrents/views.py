8f937a4e7e1bb3229bec4d083670c5fe52714e63
tevtonez <tevtonez@gmail.com>
2018-05-21 23:30:55 +0200

    #1230 - Keep email address populated after error

--- openCurrents/views.py
+++ openCurrents/views.py
@@ -568,6 +568,7 @@ class LoginView(TemplateView):
         """Get context data."""
         context = super(LoginView, self).get_context_data(**kwargs)
         context['next'] = self.request.GET.get('next', None)
+        context['user_login_email'] = self.kwargs.get('user_login_email')
 
         # adding 'next' to session
         self.request.session['next'] = context['next']
@@ -5027,11 +5028,19 @@ def process_login(request):
             }
             glogger.log_struct(glogger_struct, labels=glogger_labels)
 
-            return redirect(
-                'openCurrents:login',
-                status_msg='Invalid login/password.',
-                msg_type='alert'
-            )
+            if User.objects.filter(email=user_name).exists():
+                return redirect(
+                    'openCurrents:login',
+                    status_msg ='Invalid login or password.',
+                    msg_type ='alert',
+                    user_login_email = user_name
+                )
+            else:
+                return redirect(
+                    'openCurrents:login',
+                    status_msg ='Invalid login or password.',
+                    msg_type ='alert'
+                )
     else:
         logger.error(
             'Invalid login: %s',
