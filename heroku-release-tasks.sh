d4fa2ec23a4e7eddc24f5d75ef8c530c58d02cfb
Nickolay Shestopalov <nickolay@edyn.com>
2018-06-14 15:34:00 -0700

    base64 heroku

--- heroku-release-tasks.sh
+++ heroku-release-tasks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo $GOOGLE_APPLICATION_CREDENTIALS_KEY | base64 --decode -o gcloud-key.json
+echo $GOOGLE_APPLICATION_CREDENTIALS_KEY | base64 --decode - > gcloud-key.json
 cat ./gcloud-key.json
 ./manage.py migrate
